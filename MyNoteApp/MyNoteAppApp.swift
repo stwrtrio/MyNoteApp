a01191802f3d8f082a9ae2190ff171a5b191ac57
stewart rio <stwrtrio@gmail.com>
2025-01-17 15:00:42 +0700

    Initial Commit

---
+++ MyNoteApp/MyNoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNoteAppApp.swift
+//  MyNoteApp
+//
+//  Created by stewart rio on 17/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MyNoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyNoteAppTests/MyNoteAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyNoteAppTests.swift
+//  MyNoteAppTests
+//
+//  Created by stewart rio on 17/01/25.
+//
+
+import Testing
+@testable import MyNoteApp
+
+struct MyNoteAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
